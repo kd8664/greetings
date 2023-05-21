df45819e3700ba02e045ac0aa50c3b83fd949a96
kd8664 <kirill.dzhetpisov@mail.ru>
2023-05-21 14:53:31 +0500

    outpit data from db

--- index.php
+++ index.php
@@ -33,15 +33,36 @@ $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 echo $OUTPUT->header();
 $messageform = new \local_greetings\form\message_form();
+if ($data = $messageform->get_data()) {
+    $message = required_param('message', PARAM_TEXT);
+
+    if (!empty($message)) {
+        $record = new stdClass;
+        $record->message = $message;
+        $record->timecreated = time();
+
+        $DB->insert_record('local_greetings_messages', $record);
+    }
+}
 if (isloggedin()) {
     echo local_greetings_get_greeting($USER);
 } else {
     echo get_string('greetinguser', 'local_greetings');
 }
 $messageform->display();
-if ($data = $messageform->get_data()) {
-    $message = required_param('message', PARAM_TEXT);
+$messages = $DB->get_records('local_greetings_messages');
+echo $OUTPUT->box_start('card-columns');
 
-    echo $OUTPUT->heading($message, 4);
+foreach ($messages as $m) {
+    echo html_writer::start_tag('div', array('class' => 'card'));
+    echo html_writer::start_tag('div', array('class' => 'card-body'));
+    echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::start_tag('p', array('class' => 'card-text'));
+    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
+    echo html_writer::end_tag('p');
+    echo html_writer::end_tag('div');
+    echo html_writer::end_tag('div');
 }
+
+echo $OUTPUT->box_end();
 echo $OUTPUT->footer();
